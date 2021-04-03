dc7b564cd0f8005997c0a19010132ede5ff426fe
Jakiboy <j.sinnaour.official@gmail.com>
2021-04-03 18:52:09 +0100

    Update

--- src/AbstractBackgroundProcess.php
+++ src/AbstractBackgroundProcess.php
@@ -2,7 +2,7 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : VanillePluginTask
- * @version   : 0.1.2
+ * @version   : 0.1.3
  * @copyright : (c) 2018 - 2021 JIHAD SINNAOUR <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/VanillePluginTask/
  * @license   : MIT
